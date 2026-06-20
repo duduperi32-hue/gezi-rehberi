902e24d57f1ebc3f16a9d0f25e078a22502c6386
Kullanıcı Adınız <mail@adresiniz.com>
2026-06-20 15:53:12 +0300

    feat: add 12 new places and fetch true Wikipedia images for all new places and Kadikoy Bull

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'istanbul-gezisi-v14'; // Bump to v14 to FORCE update
+const CACHE_NAME = 'istanbul-gezisi-v15'; // Bump to v15 to FORCE update
 const urlsToCache = [
   './',
   './index.html',
